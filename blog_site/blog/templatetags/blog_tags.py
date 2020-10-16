c3100fead16cd4b8c36d6baf8be362632685e635
amerikranian <olegjpittman@gmail.com>
2020-10-16 06:53:21 -0500

    Added the show_latest_posts tag which displays the specified number of latest posts

--- blog_site/blog/templatetags/blog_tags.py
+++ blog_site/blog/templatetags/blog_tags.py
@@ -8,3 +8,13 @@ register = template.Library()
 def total_posts():
     """Returns the number of total posts on the site"""
     return Post.published.count()
+
+
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count=5):
+    """Shows the most recent posts
+    Parameters:
+        count (int): The amount of posts to show
+    """
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
